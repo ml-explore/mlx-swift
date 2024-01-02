c7d8399290d83413565766e2331e83c42a0cc334
David Koski <dkoski@apple.com>
2024-01-02 09:55:06 -0800

    encapsulate in @main

--- Source/Examples/Example1.swift
+++ Source/Examples/Example1.swift
@@ -1,8 +1,13 @@
 import Mlx
 
-let data : [Int32] = [2, 4, 6, 8]
-let arr = Array(data, [2, 2])
-print(arr)
-print(arr.dtype())
-print(arr.shape())
-print(arr.asType(DType.int64))
+@main
+struct Example1 {
+    static func main() {
+        let data : [Int32] = [2, 4, 6, 8]
+        let arr = Array(data, [2, 2])
+        print(arr)
+        print(arr.dtype())
+        print(arr.shape())
+        print(arr.asType(DType.int64))
+    }
+}
