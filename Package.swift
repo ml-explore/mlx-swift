cb576fc873c99bb8fc272ed44a1c9a78c60f926a
David Koski <dkoski@apple.com>
2024-01-27 12:56:11 -0800

    swift-tools-version has to be the first line

--- Package.swift
+++ Package.swift
@@ -1,7 +1,6 @@
-// Copyright © 2024 Apple Inc.
-
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// Copyright © 2024 Apple Inc.
 
 import PackageDescription
 
