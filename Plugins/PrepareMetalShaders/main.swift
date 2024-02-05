5d801e0d4901a5c3797b233a573ce092ea0dcf8e
David Koski <dkoski@apple.com>
2024-02-05 11:55:45 -0800

    improve handling of adverse conditions for shader build
    - empty directory (observed when submodules not checked out)
    - new files, updated files in source area (observed when updating mlx dependency)

--- Plugins/PrepareMetalShaders/main.swift
+++ Plugins/PrepareMetalShaders/main.swift
@@ -3,6 +3,14 @@
 import Foundation
 import PackagePlugin
 
+let debug = false
+
+private func log(_ message: @autoclosure () -> String) {
+    if debug {
+        print(message())
+    }
+}
+
 /// Prepare the metal shaders source for compilation.
 ///
 /// The metal shaders (`mlx/backend/metal/kernels`) include headers as:
@@ -46,6 +54,93 @@ struct PrepareMetalShaders: BuildToolPlugin {
         try new.write(to: url, atomically: true, encoding: .utf8)
     }
 
+    func collectFiles(from directory: URL) throws -> [String: Date] {
+        var result = [String: Date]()
+
+        let prefixCount = directory.pathComponents.count
+
+        if let enumerator = FileManager.default.enumerator(
+            at: directory,
+            includingPropertiesForKeys: [.isRegularFileKey, .contentModificationDateKey],
+            options: [.skipsHiddenFiles, .skipsPackageDescendants])
+        {
+
+            for case let url as URL in enumerator {
+                let resourceValues = try url.resourceValues(forKeys: [
+                    .isRegularFileKey, .contentModificationDateKey,
+                ])
+                let isRegularFile = resourceValues.isRegularFile ?? false
+
+                // ignore directories and CMakeLists.txt
+                guard isRegularFile else {
+                    continue
+                }
+                guard url.lastPathComponent != "CMakeLists.txt" else {
+                    continue
+                }
+
+                let modDate = resourceValues.contentModificationDate ?? Date()
+
+                // these will be moved to the top level (see below in building)
+                if url.pathExtension == "metal" {
+                    result[url.lastPathComponent] = modDate
+                } else {
+                    let path = url.pathComponents.dropFirst(prefixCount).joined(separator: "/")
+                    result[path] = modDate
+                }
+            }
+        }
+
+        return result
+    }
+
+    func shouldCopy(from source: URL, to destination: URL) throws -> Bool {
+        // directory does not exist
+        if !FileManager.default.fileExists(atPath: destination.path(percentEncoded: false)) {
+            log("\(destination) does not exist -- copy source metal files")
+            return true
+        }
+
+        let sourceFiles = try collectFiles(from: source)
+        if let destinationFiles = try? collectFiles(from: destination) {
+
+            log("source: \(source)")
+            log("destination: \(destination)")
+            for (path, date) in sourceFiles {
+                if let destinationDate = destinationFiles[path] {
+                    log("\(path): \(date) vs \(destinationDate)")
+                } else {
+                    log("\(path): \(date) vs MISSING")
+                }
+            }
+
+            // if there are missing files in the destination
+            if Set(sourceFiles.keys) != Set(destinationFiles.keys) {
+                print(
+                    "files in \(source) are different than in \(destination) -- copy source metal files"
+                )
+                return true
+            }
+
+            // or if there are newer files in the source
+            for (path, date) in sourceFiles {
+                if let destinationDate = destinationFiles[path] {
+                    if destinationDate < date {
+                        print("\(path) in \(destination) is out of date")
+                        return true
+                    }
+                }
+            }
+
+            print("metal files in \(destination) are up to date")
+            return false
+
+        } else {
+            // no destination
+            return true
+        }
+    }
+
     func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
         var commands = [Command]()
 
@@ -56,7 +151,10 @@ struct PrepareMetalShaders: BuildToolPlugin {
         let destination = URL(fileURLWithPath: destinationPath.string)
 
         // only do the work if the directory doesn't exist
-        if !FileManager.default.fileExists(atPath: destination.path(percentEncoded: false)) {
+        if try shouldCopy(from: source, to: destination) {
+            // remove the destination directory first in case files have been removed
+            try? FileManager.default.removeItem(at: destination)
+
             // copy the files from the source area
             try FileManager.default.createDirectory(
                 at: destination.deletingLastPathComponent(), withIntermediateDirectories: true)
