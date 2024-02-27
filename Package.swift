326c26c5f1067a2b52b293b98608d757f3f0c551
Sven A. Schmidt <sas@finestructure.co>
2024-02-27 16:52:30 +0100

    Update formatting

--- Package.swift
+++ Package.swift
@@ -169,7 +169,9 @@ let package = Package(
     cxxLanguageStandard: .gnucxx17
 )
 
-if Context.environment["MLX_SWIFT_BUILD_DOC"] == "1" || Context.environment["SPI_GENERATE_DOCS"] == "1" {
+if Context.environment["MLX_SWIFT_BUILD_DOC"] == "1"
+    || Context.environment["SPI_GENERATE_DOCS"] == "1"
+{
     // docc builder
     package.dependencies.append(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
