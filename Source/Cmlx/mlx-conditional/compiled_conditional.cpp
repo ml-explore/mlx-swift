320bf53ba2cd60c9efc3d641be402011cc95a640
Florent Morin <florentmorin@icloud.com>
2024-03-28 19:00:01 +0100

    Append visionOS support

--- Source/Cmlx/mlx-conditional/compiled_conditional.cpp
+++ Source/Cmlx/mlx-conditional/compiled_conditional.cpp
@@ -3,7 +3,7 @@
 #include <TargetConditionals.h>
 
 // select the correct cpu compile system based on TARGET_OS
-#if TARGET_OS_IOS
+#if TARGET_OS_IOS || TARGET_OS_VISION
 #include "../mlx/mlx/backend/common/compiled_nocpu.cpp"
 #else
 #include "../mlx/mlx/backend/common/compiled_cpu.cpp"
