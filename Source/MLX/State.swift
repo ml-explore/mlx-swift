df5d5c7427d68e634976a707bf1965a40cc2b2cf
David Koski <46639364+davidkoski@users.noreply.github.com>
2025-04-18 09:54:15 -0700

    various Random improvements (#219)
    * various Random improvements
    
    - allow a Key or a RandomState to be used with Random functions
    - users can create RandomState
    - users can do withRandomState to scope the use of a RandomState -- useful with multiple threads

--- Source/MLX/State.swift
+++ Source/MLX/State.swift
@@ -2,19 +2,43 @@
 
 import Foundation
 
+/// Protocol for types that can be used as a provider of random keys, e.g. for ``MLXRandom``.
+public protocol RandomStateOrKey {
+    func asRandomKey() -> MLXArray
+}
+
+extension MLXArray: RandomStateOrKey {
+    public func asRandomKey() -> MLXArray {
+        self
+    }
+}
+
 extension MLXRandom {
-    /// Global random state.
+
+    /// Random state factory.
+    ///
+    ///
     ///
     /// Note: although this type is thread-safe, the MLXArrays that it returns are not -- do not
     /// evaluate these values or expressions that depend on them across multiple threads
     /// simultaneously.
-    public class RandomState: Updatable, Evaluatable, @unchecked (Sendable) {
+    ///
+    /// ### See Also
+    /// - ``globalState``
+    /// - ``withRandomState(_:body:)-6i2p1``
+    public class RandomState: RandomStateOrKey, Updatable, Evaluatable, @unchecked (Sendable) {
         private var state: MLXArray
         private let lock = NSLock()
 
-        init() {
+        /// Initialize the RandomState with a seed based on the current time.
+        public init() {
             let now = mach_approximate_time()
             state = MLXRandom.key(now)
+        }
+
+        /// Initialize the RandomState with the given seed value.
+        public init(seed: UInt64) {
+            state = MLXRandom.key(seed)
         }
 
         public func innerState() -> [MLXArray] {
@@ -23,6 +47,7 @@ extension MLXRandom {
             }
         }
 
+        /// Split the current state and return a new Key.
         public func next() -> MLXArray {
             lock.withLock {
                 let (a, b) = MLXRandom.split(key: state)
@@ -31,10 +56,15 @@ extension MLXRandom {
             }
         }
 
+        /// Reset the random state.
         public func seed(_ seed: UInt64) {
             lock.withLock {
                 state = MLXRandom.key(seed)
             }
+        }
+
+        public func asRandomKey() -> MLXArray {
+            next()
         }
     }
 
@@ -46,4 +76,33 @@ extension MLXRandom {
     /// - ``seed(_:)``
     public static let globalState = RandomState()
 
+    /// See ``withRandomState(_:body:)`` and ``resolve(key:)``
+    @TaskLocal
+    static fileprivate var taskLocalRandomState: MLXRandom.RandomState?
+
 }  // MLXRandom
+
+/// Resolve the given key to a concrete MLXArray (random key).
+///
+/// This will use the following values (in order until one is found) to resolve the
+/// random key:
+///
+/// - the passed key, either an ``MLXArray`` or ``MLXRandom/RandomState``
+/// - the task-local ``MLXRandom/RandomState``, see ``withRandomState(_:body:)-6i2p1``
+/// - the global RandomState, ``MLXRandom/globalState``
+public func resolve(key: RandomStateOrKey?) -> MLXArray {
+    key?.asRandomKey() ?? MLXRandom.taskLocalRandomState?.asRandomKey()
+        ?? MLXRandom.globalState.next()
+}
+
+/// Use the given ``MLXRandom/RandomState`` scoped to the current task and body.
+public func withRandomState<R>(_ state: MLXRandom.RandomState, body: () throws -> R) rethrows -> R {
+    try MLXRandom.$taskLocalRandomState.withValue(state, operation: body)
+}
+
+/// Use the given ``MLXRandom/RandomState`` scoped to the current task and body.
+public func withRandomState<R>(_ state: MLXRandom.RandomState, body: () async throws -> R)
+    async rethrows -> R
+{
+    try await MLXRandom.$taskLocalRandomState.withValue(state, operation: body)
+}
