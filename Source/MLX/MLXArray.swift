598ea73cbd28fcd41e030d3217120bfd69c21d7c
David Koski <dkoski@apple.com>
2026-09-10 10:00:57 -0700

    fix build issue

--- Source/MLX/MLXArray.swift
+++ Source/MLX/MLXArray.swift
@@ -554,7 +554,7 @@ public final class MLXArray {
     /// MLX is lazy and arrays are not fully realized until they are evaluated.  This method is typically
     /// not needed as all reads ensure the contents are evaluated.
     public func eval() {
-        eval([self])
+        MLX.eval(self)
     }
 
     /// Replace the contents with a reference to a new array (INTERNAL).
